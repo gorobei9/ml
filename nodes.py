b6669fa1d820520f2a9f7e197d2ebf37a8da6e1c
Michael Naunton <michaelnaunton@gmail.com>
2017-04-10 06:00:20 -0400

    checkpoint

--- nodes.py
+++ nodes.py
@@ -26,14 +26,10 @@ class Node(object):
         return 0
             
 class Datasets(Node):
-    def __init__(self, name, raw):
+    def __init__(self, name):
         self.inp = None
         self.name = name
         super(Datasets, self).__init__(None)
-        self.raw = raw
-        self.test       = DataSet(raw.test)
-        self.validation = DataSet(raw.validation)
-        self.train      = DataSet(raw.train)
         self.x = tf.placeholder(tf.float32, shape=[None, self.shape()[-1]])
         self.y_ = tf.placeholder(tf.float32, shape=[None, self.nLabels()])
         self._out = self.x
@@ -41,9 +37,9 @@ class Datasets(Node):
     def format(self):
         return 'NC'
     def dims(self):
-        return { 'N': -1, 'C': 784 }
+        return { 'N': -1, 'C': self.train.shape()[-1] }
     def nLabels(self):
-        return self.raw.train.labels.shape[-1]
+        return self.train.labels().shape[-1]
     
     def info(self):
         print 'Dataset:', self.name
@@ -58,81 +54,145 @@ class DataSet(object):
         self.inp = inp
     def shape(self):
         return self.inp.images.shape
-       
-
-def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
+    
+    def next_batch(self, n):
+        return self.inp.next_batch(n)
+    def images(self):
+        return self.inp.images
+    def labels(self):
+        return self.inp.labels
+    
+class MNIST(Datasets):
+    def __init__(self):
+        from tensorflow.examples.tutorials.mnist import input_data
+        raw = input_data.read_data_sets('MNIST_data', one_hot=True)
+        self.raw = raw
+        self.test       = DataSet(raw.test)
+        self.validation = DataSet(raw.validation)
+        self.train      = DataSet(raw.train)
+        super(MNIST, self).__init__('MNIST')
+        
+def weight_variable(shape, sd=.1):
+  initial = tf.truncated_normal(shape, stddev=sd)
   return tf.Variable(initial)
 
-def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
+def bias_variable(shape, m=.1):
+  initial = tf.constant(m, shape=shape)
   return tf.Variable(initial)
 
 class Conv2d(Node):
-    def __init__(self, inp, size, outChannels, stride=1):
+    def __init__(self, inp, size, outChannels, stride=1, sd=.1, padding='SAME', w=None):
         self.size = size
         self.outChannels = outChannels
         self.stride = stride
+        self.padding = padding
         self.ncIn = inp.dims()['C']
-        self.W = weight_variable((size, size, self.ncIn, outChannels))
+        if w is not None:
+            self.W = w
+        else:
+            self.W = weight_variable((size, size, self.ncIn, outChannels), sd=sd)
         super(Conv2d, self).__init__(inp)
         
         x = self.inp.out()
         W = self.W
         stride = self.stride
-        self._out = tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding='SAME') 
+        self._out = tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding=padding) 
 
     def dims(self):
         d = self.inp.dims().copy()
         d['C'] = self.outChannels
+        padding = self.padding
+        reduce = 0 if self.padding == 'SAME' else self.size-1
+        def f(sz):
+            v = sz-reduce
+            return (v+self.stride-1) / self.stride
+        d['W'] = f(d['W'])
+        d['H'] = f(d['H'])
+        return d
         return d
           
     def nParam(self):
         return self.size * self.size * self.ncIn * self.outChannels
     
 class Bias(Node):
-    def __init__(self, inp):
+    def __init__(self, inp, m=.1):
         sz = inp.dims()['C']
-        self.b = bias_variable([sz])
+        self.b = bias_variable([sz], m)
         super(Bias, self).__init__(inp)
     def out(self):
         return self.inp.out() + self.b
     def nParam(self):
         return self.inp.dims()['C']
     
-class MaxPool(Node):
-    def __init__(self, inp, size, stride=1):
+class Pool(Node):
+    def __init__(self, inp, size, stride=1, padding='SAME'):
         self.size = size
         self.stride = stride
-        self.padding = 'SAME'
-        super(MaxPool, self).__init__(inp)
+        self.padding = padding
+        super(Pool, self).__init__(inp)
         
         x = self.inp.out()
         size = self.size
         stride = self.stride
         padding = self.padding
-        self._out = tf.nn.max_pool(x, ksize=[1, size, size, 1], strides=[1, stride, stride, 1], padding=padding)
+        fn = self._fn[0]
+        self._out = fn(x, ksize=[1, size, size, 1], strides=[1, stride, stride, 1], padding=padding)
         
     def dims(self):
         d = self.inp.dims().copy()
-        size = self.size
-        d['W'] = d['W'] / self.stride
-        d['H'] = d['H'] / self.stride
+        padding = self.padding
+        reduce = 0 if self.padding == 'SAME' else self.size-1
+        def f(sz):
+            v = sz-reduce
+            return (v+self.stride-1) / self.stride
+        d['W'] = f(d['W'])
+        d['H'] = f(d['H'])
         return d
 
+class MaxPool(Pool):
+    _fn = [tf.nn.max_pool]
+    
+class AvgPool(Pool):
+    _fn = [tf.nn.avg_pool]
+    
 class Reshape(Node):
     def __init__(self, inp, channel, outChannels, outShape):
+        self.channel = channel
+        self.outChannels = outChannels
+        self.outShape = outShape
         super(Reshape, self).__init__(inp)
         inNode = self.inp
         self._out = tf.reshape(inNode.out(), self.shape())
         
     def format(self):
-        return 'NWHC'
+        inFmt = self.inp.format()
+        ret = []
+        for d in inFmt:
+            ret.append(d if d != self.channel else self.outChannels)
+        return ''.join(ret)
     def dims(self):
-        return { 'N': -1, 'H': 28, 'W': 28, 'C': 1 }
-    def shape(self):
-        return tuple([ self.dims()[v] for v in self.format()] )
+        ret = dict(zip(self.outChannels, self.outShape))
+        for d, s in self.inp.dims().items():
+            if d not in ret:
+                ret[d] = s
+        return ret
+    #def shape(self):
+    #    return tuple([ self.dims()[v] for v in self.format()] )
 
+class Transpose(Node):
+    def __init__(self, inp, fmt):
+        t = []
+        f = inp.format()
+        for d in fmt:
+            t.append(f.find(d))
+        self.fmt = fmt
+        super(Transpose, self).__init__(inp)
+        self._out = tf.transpose(self.inp.out(), t)
+    def format(self):
+        return self.fmt
+    def dims(self):
+        return self.inp.dims()
+    
 class Relu(Node):
     def __init__(self, inp):
         super(Relu, self).__init__(inp)
@@ -141,7 +201,7 @@ class Relu(Node):
         return self.inp.dims()
     def format(self):
         return self.inp.format()
-
+    
 class Softmax(Node):
     def __init__(self, inp):
         super(Softmax, self).__init__(inp)
@@ -196,7 +256,7 @@ class Accuracy(Node):
         correct_prediction = tf.equal(tf.argmax(inp.out(), 1), tf.argmax(y_, 1))
         self._out = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     def dims(self):
-        return { 'N': -1, 'C': 1 }
+        return { 'N': 1, 'C': 1 }
 
 class CrossEntropy(Node):
     def __init__(self, inp, y_):
--- traces.py
+++ traces.py
@@ -7,16 +7,31 @@ def inds(n, sz=10):
     v = [ int(float(i)*(n-1)/(sz-1)+.49) for i in r ]
     return v
 
+def inds2(n):
+    r = []
+    i = 1
+    while i < n/2:
+        r.append(i)
+        i = i * 2
+    return r + [ -i for i in reversed(r) ]
+
 class Trace(object):
-    def __init__(self, inp, name, yFn=None):
+    def __init__(self, inp, name, yFn=None, skip=0):
         self.inp = inp
         self.name = name
         self.yFn = yFn
         self.data = []
         self.mbData = []
+        self.skip = skip
+        self.fn = None
     def sample(self, v, sz=10):
         return v[inds(len(v), sz=sz)]
+    def mapE(self, v):
+        return v
     def addEpoch(self, v):
+        v = self.mapE(v)
+        if self.fn == '(pow2 bins)':
+            v = v[inds2(len(v))]
         if self.mbData:
             v2 = np.mean(self.mbData)
             v = (v2, v)
@@ -25,52 +40,81 @@ class Trace(object):
     def addBatch(self, v):
         pass
     def plot(self):
-        if self.yFn == 'log':
-            plt.semilogy(self.data)
+        if len(self.data) <= self.skip:
+            return
+        d = np.array(self.data[self.skip:])
+        y = [ e+self.skip for e in range(len(d)) ]
+
+        if self.fn == '(pow2 bins)':
+            c = ('bgcm' * 10 + 'rk')[-len(d[0])/2:]
+            c = c + ''.join(list(reversed(c)))
+
+            if self.yFn == 'log':
+                for i, cc in enumerate(c):
+                    dd = d[:, i]
+                    plt.semilogy(y, dd, color=cc)
+            else:
+                for i, cc in enumerate(c):
+                    dd = d[:, i]
+                    plt.plot(y, dd, color=cc)
+
         else:
-            plt.plot(self.data)
+            if self.yFn == 'log':
+                plt.semilogy(y, d)
+            else:
+                plt.plot(y, d)
+
+    def title(self):
+        s = self.name
+        if self.fn:
+            s = s + ' ' + self.fn
+        if self.yFn:
+            s = s + ', log'
+        if self.skip:
+            s = s + ' (first %s skipped)' % self.skip
+        return s
     
 class ScalarTrace(Trace):
     def __init__(self, inp, name, **k):
         super(ScalarTrace, self).__init__(inp, name, **k)
     def addBatch(self, v):
         self.mbData.append(v)
-        
+  
 class BiasTrace(Trace):
     def __init__(self, inp, name, **k):
         super(BiasTrace, self).__init__(inp, name, **k)
-    def addEpoch(self, v):
+        self.fn = '(pow2 bins)'
+    def mapE(self, v):
         v = np.sort(v.flatten())
-        v = self.sample(v)
-        self.data.append(v)
-
+        return v
+    
 class WeightTrace(Trace):
     def __init__(self, inp, name, **k):
         super(WeightTrace, self).__init__(inp, name, **k)
-    def addEpoch(self, v):
+        self.fn = '(pow2 bins)'
+    def mapE(self, v):
         v = np.sort(v.flatten())
-        v = self.sample(v, 40)
-        self.data.append(v)
+        return v
         
-class SMTrace(Trace):
+class OutTrace(Trace):
     def __init__(self, inp, name, **k):
-        super(SMTrace, self).__init__(inp, name, **k)
-    def addEpoch(self, v):
-        v = np.sort(v)
+        super(OutTrace, self).__init__(inp, name, **k)
+        self.fn = '(pow2 bins)'
+    def mapE(self, v):
+        v = np.sort(v.reshape([v.shape[0]]+[-1]))
         v = np.mean(v, axis=0)
-        v = self.sample(v)
-        self.data.append(v)
-
-class SMTopTrace(Trace):
+        return v
+    
+class TopTrace(Trace):
     def __init__(self, inp, name, index=-1):
         self.index = index
-        super(SMTopTrace, self).__init__(inp, name)
-    def addEpoch(self, v):
-        v = np.sort(v)
+        super(TopTrace, self).__init__(inp, name)
+        self.fn = '(pow2 bins)'
+    def mapE(self, v):
+        v = np.sort(v.reshape([v.shape[0]]+[-1]))
         v = v[:,self.index]
         v = np.sort(v)
-        v = self.sample(v)
-        self.data.append(v)
+        return v
 
 
 class Track(object):
@@ -91,8 +135,9 @@ class Track(object):
             for x, p in enumerate(t):
                 i = y * c + x + 1
                 plt.subplot(r, c, i)
-                plt.title(p.name)
-                p.plot()    
+                plt.title(p.title())
+                p.plot()
+        plt.tight_layout()
         plt.show()
     def addBatch(self, vals):
         for t, v in zip(self.tFlat, vals):
